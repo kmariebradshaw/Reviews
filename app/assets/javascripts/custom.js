26ecf43d34e5437932ce2d57b12d0ac129e7ee29
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2019-02-25 11:31:13 -0600

    adds a rotating review function for staff favorites on the published reviews page

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -13,3 +13,32 @@ function setNavigation () {
     }
   })
 }
+
+$('body').on("click", function() {
+  console.log('clicked')
+
+})
+
+
+$('document').ready(function() {
+
+  setInterval(function() {
+    if ($('.staff-favorites:visible').next().length > 0) {
+      $('.current-review').removeClass('current-review').next().addClass('current-review')
+      $('.staff-favorites:visible').hide().next().show();
+    }
+    else {
+      $('.staff-favorites:visible').hide()
+      $('.current-review').removeClass('current-review')
+      $('#0').show();
+      $('.circle').first().addClass('current-review')
+    }
+  }, 10000)
+  $('.circle').click(function() {
+
+    $(this).addClass('current-review').siblings().removeClass('current-review')
+    var requestedReview = $(this).attr("data-review-source")
+    $('.staff-favorites:visible').hide()
+    $(requestedReview).show(); 
+  })
+})
