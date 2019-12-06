cd22a3cc59bbb285eab65b42bdd70a4205dbb120
Kate Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2019-12-06 12:45:47 -0600

    creates a page to edit and delete products and the associated routes but not views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,9 +17,22 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all 
   end 
+  def edit
+    @product = Product.find(params[:id])
+  end 
   def update
     @product = Product.find(params[:id]) 
+     if @product.update(product_params)
+      redirect_to '/products'
+     else
+      render 'edit'
+    end
   end 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy 
+    redirect_to '/products'
+  end
   private 
   def product_params
       params.require(:product).permit(:title)
